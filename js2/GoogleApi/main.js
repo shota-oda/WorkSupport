bab508caa0a2525d574eb0e9b94b6d20c3603f68
shota-oda <Shota@MacBook-Air.local>
2016-04-26 18:53:54 +0900

    test

--- js2/GoogleApi/main.js
+++ js2/GoogleApi/main.js
@@ -56,7 +56,7 @@ WorkGadget.gApi = WorkGadget.gApi || {};
       // auth is fail
       self.status.isAuthorized = false;
       $authButton.on("click", function () {
-        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, self.handleAuth);
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, self.handleAuth);
       })
     }
   }
