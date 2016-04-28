50fa02c8cd14fa503658bcdf8fbc14a4fec101d5
shota-oda <Shota@MacBook-Air.local>
2016-04-29 00:23:22 +0900

    modify start fn

--- js2/Core/WorkGadget.js
+++ js2/Core/WorkGadget.js
@@ -13,17 +13,41 @@ function main () {
     WorkGadget.gApi.status.isLibraryReady = true
     WorkGadget.gApi.init()
 
+	function appStart(){
+	    WorkGadget.App = new Backbone.Marionette.Application()
+		
+		WorkGadget.App.on('before:start', function () {
+
+			WorkGadget.App.View = {};
+			WorkGadget.App.View.Root = new WorkGadget.View.RootLayoutView();
+		});
+
+		WorkGadget.App.on('start', function () {
+			
+			var controller = new WorkGadget.Controller();
+			controller.router = new WorkGadget.Router({
+				controller: controller
+			});
+
+			Backbone.history.start();
+		});
+
+		WorkGadget.App.start();
+	}
+
     //silently check authed or not
     WorkGadget.Common.fn.DoAsync(WorkGadget.gApi.checkAuth(true, function(result) {
     	$load.hide();
     	if (result){
     		$filter.hide();
+    		appStart();
     	} else {
     		$authButton.on("click", function(){
     			WorkGadget.gApi.checkAuth(false, function(result) {
     				if (result){
     					$authButton.hide()
-    					$filter.hide()
+    					$filter.hide();
+    					appStart();
     			});
     		});
     		$authButton.show();
@@ -31,23 +55,4 @@ function main () {
     }));
 
 
-    WorkGadget.App = new Backbone.Marionette.Application()
-	
-	WorkGadget.App.on('before:start', function () {
-
-		WorkGadget.App.View = {};
-		WorkGadget.App.View.Root = new WorkGadget.View.RootLayoutView();
-	});
-
-	WorkGadget.App.on('start', function () {
-		
-		var controller = new WorkGadget.Controller();
-		controller.router = new WorkGadget.Router({
-			controller: controller
-		});
-
-		Backbone.history.start();
-	});
-
-	WorkGadget.App.start();
 }
