ff4b07e4c296feafc992fbfb86f3bee35f9ecd85
shota-oda <Shota@MacBook-Air.local>
2016-04-29 01:16:31 +0900

    fix miss

--- js2/DailyReport/WorkGadget.Model.DailyReport.js
+++ js2/DailyReport/WorkGadget.Model.DailyReport.js
@@ -34,14 +34,14 @@ var WorkGadget = WorkGadget || {};
 
 			this.col1 = this.getColumn(1, '勤怠', this.getDateString() + '\n出勤:' + (this.isMonday() ? '08:30' : '09:30') + '\n退社:' + (this.isMonday() ? '17:30' : '18:30'));
 			
-			WorkGadget.gApi.Calendar.getTodayTask()
+			WorkGadget.gApi.calendar.getTodayTask()
 				.done(function (tts){
 					this.set("col2", this.getColumn(2, "本日の業務", tts.reduce(function(p, c){
 						return p + '\n' + c;
 					})));
 				});
 
-			WorkGadget.gApi.Calendar.getTommorrowTask()
+			WorkGadget.gApi.calendar.getTommorrowTask()
 				.done(function (tts){
 					this.set("col3", this.getColumn(2, "明日の業務と直近の主な完了予定", tts.reduce(function(p, c){
 						return p + '\n・' + c;
