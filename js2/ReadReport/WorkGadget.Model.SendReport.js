a9eae35963d1b8a33efc8ce4cc3cd1cdc007d429
shota-oda <shota.oda@bizreach.co.jp>
2016-05-09 22:27:51 +0900

    add

---
+++ js2/ReadReport/WorkGadget.Model.SendReport.js
@@ -0,0 +1,106 @@
+/*global Backbone, WorkGadget:true */
+
+var WorkGadget = WorkGadget || {};
+
+(function () {
+	'use strict';
+
+	WorkGadget.Model = WorkGadget.Model || {};
+	
+	// SendReport Model
+	// ----------
+	WorkGadget.Model.SendReport = Backbone.Model.extend({
+		defaults: {
+			to:'',
+			cc: '',
+			subject: '',
+			col1: '',
+			col2: '',
+			col3: '',
+			col4: '',
+			col5: '',
+			cal: {},//For Consistency, set Date() to member
+		},
+
+		initialize: function () {
+			this.cal = new Date();
+			
+			this.set('subject', this.getSubject())
+			this.set('to', 'daily_report_rookie2016@bizreach.co.jp');
+			this.set('cc', 'rookie_2016@bizreach.co.jp');
+
+			this.col1 = this.getColumn(1, '勤怠', this.getDateString() + '\n出勤:' + (this.isMonday() ? '08:30' : '09:30') + '\n退社:' + (this.isMonday() ? '17:30' : '18:30'));
+			
+			//for use this in done callback
+			var thisModel = this;
+			WorkGadget.gApi.calendar.getTodayEvents()
+				.done(function (data){
+					var taskListStr = data.reduce(function(p, c){
+						return p + '\n' + c;
+					});
+
+					//thisModel.col2 = thisModel.getColumn(2, "本日の業務", taskListStr);
+					thisModel.col2 = thisModel.getColumn(2, "本日の業務", taskListStr);
+					thisModel.trigger("change");
+				});
+
+			WorkGadget.gApi.calendar.getTommorrowEvents()
+				.done(function (data){
+					var taskListStr = data.reduce(function(p, c){
+						return p + '\n' + c;
+					});
+					//thisModel.col3 = thisModel.getColumn(3, "明日の業務と直近の主な完了予定", taskListStr);
+					thisModel.col3 = thisModel.getColumn(3, "明日の業務と直近の主な完了予定", taskListStr);
+					thisModel.trigger("change");
+				});
+
+			this.col4 = this.getColumnHeader(4, '本日の気づきと学び');
+
+			this.col5 = this.getColumnHeader(5, '振り返り・明日への宣言');
+		},
+
+		updateTodayInsight: function (text) {
+			this.set({col4: this.getColumn(4, '本日の気づきと学び', text)});
+		},
+
+		getSubject: function () {
+			return '【新卒日報】' + this.getDateString() + ' 小田翔太';
+		},
+
+		getColumnHeader: function (colNum, colTitle) {
+			return '───────────────────────────\n' + colNum + '）' + colTitle + '\n' + '───────────────────────────\n';
+		},
+
+		getColumn: function (colNum, colTitle, content) {
+			return  this.getColumnHeader(colNum, colTitle) + content + '\n\n';
+		},
+
+		// -> YYYY/MM/DD(d)
+		getDateString: function () {
+			function getJaDay_Short(dayNum){
+				switch(dayNum){
+					case 0:
+					return "日"
+					case 1:
+					return "月"
+					case 2:
+					return "火"
+					case 3:
+					return "水"
+					case 4:
+					return "木"
+					case 5:
+					return "金"
+					case 6:
+					return "土"
+				}
+			}
+			return this.cal.getFullYear() + "/" + (this.cal.getMonth() + 1) + "/" + this.cal.getDate() + "(" + getJaDay_Short(this.cal.getDay()) + ")"
+		},
+
+		isMonday: function () {
+			return this.cal.getDay() === 1;
+		},
+	});
+
+})();
---
+++ js2/ReadReport/WorkGadget.View.SendReport.js
@@ -0,0 +1,62 @@
+/*global WorkGadget:true, Backbone */
+
+var WorkGadget = WorkGadget || {};
+
+(function () {
+	'use strict';
+
+	WorkGadget.View = WorkGadget.View || {};
+
+	//Send Repoert View
+	// ------------------
+	WorkGadget.View.SendReport = Backbone.Marionette.ItemView.extend({
+
+		template: '#template-SendReport',
+
+		// UI bindings create cached attributes that
+		// point to jQuery selected objects
+		ui: {
+			input: '#Report-Content',
+			send: '#Report-Send',
+			reset:'#Report-Reset',
+			preview: '#Report-Preview',
+		},
+
+		events: {
+			'keyup @ui.input': 'onInputKeyUp',
+			'click @ui.send': 'onSendClick',
+			'click @ui.reset': 'onResetClick',
+		},
+
+		modelEvents: {
+			'change': 'render',
+		},
+
+		initialize: function () {
+			
+		},
+
+		templateHelpers: function () {
+			return {
+				body: this.getBody()
+			}
+		},
+
+		getBody: function (input) {
+			return this.model.col1 + this.model.col2 + this.model.col3 + this.model.col4 + (input? input : '') + '\n\n' + this.model.col5 + '\n\n'
+		},
+
+		onInputKeyUp: function (e) {
+			var input = this.ui.input.val()
+			this.ui.preview.text(this.getBody(input));
+		},
+
+		onSendClick: function() {
+			console.log("click")
+		},
+
+		onResetClick: function () {
+			console.log("click2")
+		}
+	});
+})();
