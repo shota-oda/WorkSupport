78169d82442ebe532671d01a25a9ebc299c5858d
ShotaOda <shota.q3q2@gmail.com>
2016-05-12 18:02:11 +0900

    remove error

--- js/SendReport/WorkGadget.View.SendReport.js
+++ js/SendReport/WorkGadget.View.SendReport.js
@@ -59,29 +59,29 @@ var WorkGadget = WorkGadget || {};
 			console.log("click2")
 		},
 
-		onPreviewClick: function(this){
-			console.log(this);
+		onPreviewClick: function(e){
+			console.log(e);
 
-			$('dd').click(function(){
-	       		//classでonを持っているかチェック
-				if(!$(this).hasClass('on')){
-				    //編集可能時はclassでonをつける
-				    $(this).addClass('on');
-				    var txt = $(this).text();
-				    //テキストをinputのvalueに入れてで置き換え
-				    $(this).html('<input type="text" value="'+txt+'" />');
-				    //同時にinputにフォーカスをする
-				    $('dd > input').focus().blur(function(){
-				        var inputVal = $(this).val();
-				        //もし空欄だったら空欄にする前の内容に戻す
-				        if(inputVal===''){
-				            inputVal = this.defaultValue;
-				        };
-				        //編集が終わったらtextで置き換える
-				        $(this).parent().removeClass('on').text(inputVal);
-				    });
-				};
-			});
+			// $('dd').click(function(){
+	  //      		//classでonを持っているかチェック
+			// 	if(!$(this).hasClass('on')){
+			// 	    //編集可能時はclassでonをつける
+			// 	    $(this).addClass('on');
+			// 	    var txt = $(this).text();
+			// 	    //テキストをinputのvalueに入れてで置き換え
+			// 	    $(this).html('<input type="text" value="'+txt+'" />');
+			// 	    //同時にinputにフォーカスをする
+			// 	    $('dd > input').focus().blur(function(){
+			// 	        var inputVal = $(this).val();
+			// 	        //もし空欄だったら空欄にする前の内容に戻す
+			// 	        if(inputVal===''){
+			// 	            inputVal = this.defaultValue;
+			// 	        };
+			// 	        //編集が終わったらtextで置き換える
+			// 	        $(this).parent().removeClass('on').text(inputVal);
+			// 	    });
+			// 	};
+			// });
 		},
 	});
 })();
