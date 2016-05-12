87d513f8718a19bdc5e61e10cea9f025e6b6b55d
ShotaOda <shota.q3q2@gmail.com>
2016-05-12 17:49:24 +0900

    s

--- js/SendReport/WorkGadget.View.SendReport.js
+++ js/SendReport/WorkGadget.View.SendReport.js
@@ -24,6 +24,7 @@ var WorkGadget = WorkGadget || {};
 			'keyup @ui.input': 'onInputKeyUp',
 			'click @ui.send': 'onSendClick',
 			'click @ui.reset': 'onResetClick',
+			'click @ui.preview': 'onPreviewClick',
 		},
 
 		modelEvents: {
@@ -54,8 +55,33 @@ var WorkGadget = WorkGadget || {};
 			console.log("click")
 		},
 
-		onResetClick: function () {
+		onResetClick: function() {
 			console.log("click2")
-		}
+		},
+
+		onPreviewClick: function(this){
+			console.log(this);
+			
+			$('dd').click(function(){
+	       		//classでonを持っているかチェック
+				if(!$(this).hasClass('on')){
+				    //編集可能時はclassでonをつける
+				    $(this).addClass('on');
+				    var txt = $(this).text();
+				    //テキストをinputのvalueに入れてで置き換え
+				    $(this).html('<input type="text" value="'+txt+'" />');
+				    //同時にinputにフォーカスをする
+				    $('dd > input').focus().blur(function(){
+				        var inputVal = $(this).val();
+				        //もし空欄だったら空欄にする前の内容に戻す
+				        if(inputVal===''){
+				            inputVal = this.defaultValue;
+				        };
+				        //編集が終わったらtextで置き換える
+				        $(this).parent().removeClass('on').text(inputVal);
+				    });
+				};
+			});
+		},
 	});
 })();
