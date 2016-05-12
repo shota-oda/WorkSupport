bec5ed6f19f876f0d058ce1f4cb79ed58cebf0ea
ShotaOda <shota.q3q2@gmail.com>
2016-05-12 11:25:18 +0900

    fix namespace error

--- js/ManageSetting/WorkGadget.View.ManageSetting.js
+++ js/ManageSetting/WorkGadget.View.ManageSetting.js
@@ -9,44 +9,8 @@ var WorkGadget = WorkGadget || {};
 
 	//Send Repoert View
 	// ------------------
-	WorkGadget.View.SendReport = Backbone.Marionette.ItemView.extend({
+	WorkGadget.View.Setting = Backbone.Marionette.ItemView.extend({
 
-		template: '#template-SendReport',
-
-		// UI bindings create cached attributes that
-		// point to jQuery selected objects
-		ui: {
-			input: '#Report-Content',
-			send: '#Report-Send',
-			reset:'#Report-Reset',
-			preview: '#Report-Preview',
-		},
-
-		events: {
-			'keyup @ui.input': 'onInputKeyUp',
-			'click @ui.send': 'onSendClick',
-			'click @ui.reset': 'onResetClick',
-		},
-
-		modelEvents: {
-			'change': 'render',
-		},
-
-		initialize: function () {
-			
-		},
-
-		onInputKeyUp: function (e) {
-			var input = this.ui.input.val()
-			this.ui.preview.text(this.getBody(input));
-		},
-
-		onSendClick: function() {
-			console.log("click")
-		},
-
-		onResetClick: function () {
-			console.log("click2")
-		}
+		
 	});
 })();
