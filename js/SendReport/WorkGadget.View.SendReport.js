12027821cf0a427b5f21f3e0b58dcdf2a3e0844d
ShotaOda <shota.q3q2@gmail.com>
2016-05-12 15:44:16 +0900

    a

--- js/SendReport/WorkGadget.View.SendReport.js
+++ js/SendReport/WorkGadget.View.SendReport.js
@@ -34,13 +34,10 @@ var WorkGadget = WorkGadget || {};
 			
 		},
 
-		onShow: function(){
-			this.ui.input.text(this.model.inputTemplate)
-		},
-
 		templateHelpers: function () {
 			return {
-				body: this.getBody()
+				 inputTemplate: this.model.inputTemplate
+				,body: this.getBody()
 			}
 		},
 
